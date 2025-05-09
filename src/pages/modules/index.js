028b42c519338a228eafed8cb6a5fc044ce51034
lehosoda <lehosoda@gmail.com>
2025-05-09 13:23:23 -0700

    navbar, modules

--- src/pages/modules/index.js
+++ src/pages/modules/index.js
@@ -1,10 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const Modules = () => {
-    return (
-        <h1>Modules</h1>
-    )
-}
+  const [currentModule, setCurrentModule] = useState({
+    title: "Module 1",
+    videoId: "K4TOrB7at0Y?si=YOEONcW8w7mnW6wK",
+  });
+
+  const modules = [
+    { title: "Module 1", videoId: "K4TOrB7at0Y?si=YOEONcW8w7mnW6wK", defaultImg: "/imgs/light-rectangle.svg" },
+    { title: "Module 2", videoId: "K4TOrB7at0Y?si=YOEONcW8w7mnW6wK", defaultImg: "/imgs/light-rectangle.svg" },
+    { title: "Module 3", videoId: "K4TOrB7at0Y?si=YOEONcW8w7mnW6wK", defaultImg: "/imgs/light-rectangle.svg" },
+  ];
+  
+
+  const handleModuleClick = (module) => {
+    setCurrentModule(module);
+  };
+
+  return (
+    <div className="video-modules">
+      {/* Top Title */}
+      <div className="cyber-aware-learning-modules-module-1">
+        <span className="cyber-aware-learning-modules-module-1-span">
+          Cyber Aware Learning Modules: {" "}
+        </span>
+        <span className="cyber-aware-learning-modules-module-1-span2">
+          {currentModule.title}
+        </span>
+      </div>
+
+      {/* YouTube Video Embed */}
+      <div className="video-container">
+        <iframe
+          className="video-frame"
+          src={`https://www.youtube.com/embed/${currentModule.videoId}`}
+          title={currentModule.title}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      {/* Section Header for Modules */}
+      <div className="modules-section-header">Select a Module</div>
+
+      {/* Module Buttons */}
+      {modules.map((mod, index) => (
+        <div
+          key={mod.title}
+          onClick={() => handleModuleClick(mod)}
+          className={`module-clickable module-${index + 1}-clickable`}
+        >
+          <img
+            src={
+              currentModule.title === mod.title
+                ? "/imgs/dark-rectangle.svg"
+                : mod.defaultImg
+            }
+            alt={mod.title}
+          />
+          <div className="module-text">{mod.title}</div>
+        </div>
+      ))}
+
+      {/* Language Box */}
+      <div className="rectangle-37"></div>
+      <div className="rectangle-36"></div>
+
+      <div className="current-transcript-language-english">
+        <span className="current-transcript-language-english-span">
+          Current Transcript Language: {" "}
+        </span>
+        <span className="current-transcript-language-english-span2">English</span>
+      </div>
+
+      <div className="change-language">Change Language</div>
+    </div>
+  );
+};
 
 export default Modules;
--- src/pages/navbar/index.js
+++ src/pages/navbar/index.js
@@ -1,25 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./style.css";
 
 const NavBar = () => {
-    return (
-            <header>
-                <div className="nav-bar">
-                    <Link to="/home">
-                        <img className="logo" src="/imgs/logo.svg" alt="Logo" />
-                    </Link>
-                    <div className="navigation">
-                        <img className="learn-nav" src="/imgs/learn.svg" alt="Learn" />
-                        <Link to="/chatbot">
-                            <img className="chatbot-nav" src="/imgs/chatbot.svg" alt="Chatbot" />
-                        </Link>
-                        <Link to="/forum">
-                            <img className="forum-nav" src="/imgs/forum.svg" alt="Forum" />
-                        </Link>
-                    </div>
-                </div>
-            </header>
-    )};
+    const [menuOpen, setMenuOpen] = useState(false);
+    const location = useLocation();
 
-    export default NavBar;
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const getImage = (path, defaultImg, activeImg) => {
+        return location.pathname === path ? activeImg : defaultImg;
+    };
+
+    return (
+        <header>
+            <div className="nav-bar">
+                <Link to="/home">
+                    <img className="logo" src="/imgs/logo.svg" alt="Logo" />
+                </Link>
+                <button className="hamburger" onClick={toggleMenu}>
+                    ☰
+                </button>
+                <div className={`navigation ${menuOpen ? "active" : ""}`}>
+                    <Link to="/modules">
+                        <img 
+                            className="learn-nav" 
+                            src={getImage("/modules", "/imgs/learn.svg", "/imgs/learn-red.svg")} 
+                            alt="Learn" 
+                        />
+                    </Link>
+                    <Link to="/chatbot">
+                        <div className="chatbot-wrapper">
+                            <img 
+                                className="chatbot-nav" 
+                                src={getImage("/chatbot", "/imgs/chatbot.svg", "/imgs/chatbot-red.svg")} 
+                                alt="Chatbot" 
+                            />
+                        </div>
+                    </Link>
+                    <Link to="/forum">
+                        <img 
+                            className="forum-nav" 
+                            src={getImage("/forum", "/imgs/forum.svg", "/imgs/forum-red.svg")} 
+                            alt="Forum" 
+                        />
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default NavBar;
